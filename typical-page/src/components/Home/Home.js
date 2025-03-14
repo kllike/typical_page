24e3de0b15476ce704db75b58a3a24d8804e2e0d
Kullike Annuk <kullike.annuk@voco.ee>
2025-03-14 12:11:06 +0200

    User login and logout with localStorage

---
+++ typical-page/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import './Home.css'
+import Card from '../UI/Card'
+
+const Home = () => {
+    return (
+        <Card className="home">
+            <h1>Welcome back!</h1>
+        </Card>
+    )
+} 
+
+export default Home
